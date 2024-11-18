d00a0867c0cd132f657dbb5548d6fb327016bc91
huma-meng <mauricehufnagel@gmx.de>
2024-11-18 17:26:30 +0100

    __init__.py parts added

--- components/LD2450/__init__.py
+++ components/LD2450/__init__.py
@@ -1,6 +1,5 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-
 from esphome import automation
 from esphome.components import uart
 from esphome.components.uart import UARTComponent
@@ -8,6 +7,7 @@ from esphome.const import CONF_ID
 
 
 DEPENDENCIES = ["uart"]
+UART_ID = "uart_id"
 
 
 ld2450_ns = cg.esphome_ns.namespace("ld2450")
@@ -17,7 +17,8 @@ LD2450 = ld2450_ns.class_("LD2450", cg.Component, uart.UARTDevice)
 CONFIG_SCHEMA = cv.All(
     uart.UART_DEVICE_SCHEMA.extend(
         {
-
+            cv.GenerateID(): cv.declare_id(LD2450),
+            cv.Required(UART_ID): cv.use_id(UARTComponent),
         }
     )
 )
