1e050ae58f0aa15c04a8208fb30a398a3a3ac263
huma-meng <mauricehufnagel@gmx.de>
2024-11-18 18:26:39 +0100

    Renaming

--- components/ld2450/__init__.py
+++ components/ld2450/__init__.py
@@ -3,27 +3,26 @@ import esphome.config_validation as cv
 from esphome.components import (
     uart,
 )
-from esphome.components.uart import UARTComponent
 from esphome.const import (
     CONF_ID,
-    CONF_NAME,
 )
 
+# ld2450
+CONF_LD2450_ID = "ld2450_id"
 
 MULTI_CONF = True
 DEPENDENCIES = ['uart']
-UART_ID = "uart_id"
 
 
-ld2450_ns = cg.esphome_ns.namespace('ld2450')
-LD2450 = ld2450_ns.class_('LD2450', cg.Component, uart.UARTDevice)
+ld2450_ns = cg.esphome_ns.namespace("ld2450")
+LD2450 = ld2450_ns.class_("LD2450", cg.Component, uart.UARTDevice)
 
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(LD2450),
-        cv.Required(UART_ID): cv.use_id(UARTComponent),
-        cv.Optional(CONF_NAME, default="LD2450"): cv.string_strict,
+        # cv.Required(UART_ID): cv.use_id(UARTComponent),
+        # cv.Optional(CONF_NAME, default="LD2450"): cv.string_strict,
     }
 ).extend(uart.UART_DEVICE_SCHEMA)
 
@@ -32,3 +31,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
+
+
