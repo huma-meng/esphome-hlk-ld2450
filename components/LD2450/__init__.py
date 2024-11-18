de2082a05e3e49e525a5921d5b531192a47332ab
huma-meng <mauricehufnagel@gmx.de>
2024-11-18 17:45:32 +0100

    __init__.py update

--- components/LD2450/__init__.py
+++ components/LD2450/__init__.py
@@ -1,26 +1,22 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-
 from esphome.components import uart
-from esphome.const import CONF_ID, CONF_PORT, CONF_BAUD_RATE
+from esphome.const import CONF_ID
 
 
-DEPENDENCIES = ["uart"]
-UART_ID = "uart_id"
+DEPENDENCIES = ['uart']
 
 
-ld2450_ns = cg.esphome_ns.namespace("ld2450")
-LD2450 = ld2450_ns.class_("LD2450", cg.Component, uart.UARTDevice)
+ld2450_ns = cg.esphome_ns.namespace('ld2450')
+LD2450 = ld2450_ns.class_('LD2450', cg.Component, uart.UARTDevice)
 
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(LD2450),
-}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
-
+}).extend(uart.UART_DEVICE_SCHEMA)
 
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
